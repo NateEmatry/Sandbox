306dea690b5be73bfa945b59a37ca1b66a29d4a2
Nate Tryon <ematrygames@gmail.com>
2021-12-19 17:22:29 -0500

    Getting console up

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -1,3 +1,21 @@
 function sayHello() {
     console.log("Hello game!");
 }
+
+function keyDownJS(event) {
+
+    switch (event.keyCode)
+    {
+        // case 37:
+        case 38:
+        // case 39:
+        case 40:
+        {
+            event.preventDefault();
+        } break;
+            
+        default: break;
+    }
+
+    console.log("User pressed " + event.keyCode);
+}
